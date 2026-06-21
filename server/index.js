c18777a0844cb9c16676cfab6a26fc59b9289e59
Shivanshu Sonwani <shivanshu.ed@gmail.com>
2026-06-21 12:50:43 +0530

    implement database connection and update dependencies

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import app from './src/app.js';
+import connectDB from './src/config/db.js';
 
 const PORT = process.env.PORT || 8000;
 
-const startServer = () => {
+const startServer = async () => {
 	try {
+		await connectDB();
+
 		app.listen(PORT, () => {
 			console.log(`Server running on port ${PORT}`);
 		});
